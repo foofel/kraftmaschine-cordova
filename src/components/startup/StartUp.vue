dbe7fe324def6a5c06aa3b00606fa14d4734b082
Urbschat <urbschaj@win.desy.de>
2021-05-17 00:41:47 +0200

    upgraded typescript and dependencys, removed https dev server for now

--- src/components/startup/StartUp.vue
+++ src/components/startup/StartUp.vue
@@ -1,12 +1,32 @@
 <template>
-    <div class="maximize" id="startup">
-        <div>facebook</div>
+    <div class="h-screen w-screen flex justify-center" id="startup">
+        <!--div>facebook</div>
 		<div>instagram</div>
 		<div>
 			<div class="text-teal-200">username</div>
 			<div>password</div>
 		</div>
-		<div>name: "{{userName}}"</div>
+		<div>name: "{{userName}}"</div-->
+        <div id="header" class="">
+            kraft maschine
+        </div>
+        <div id="logins" class="">
+            logins
+        </div>
+        <div class=""></div>
+        <!--div class="grid grid-cols-1 gap-4">
+            <div class="bg-red-200 text-center">
+                <div class="text-3xl font-light inline-block">kraft</div>
+                <div class="text-2xl font-light inline-block">maschine</div>
+            </div>
+            <div class="bg-red-300 text-center gap-4">
+                <div>2</div>
+            </div>
+            <div class="bg-red-400 text-center">
+                <div>3</div>
+            </div>
+        </div-->
+        test4
     </div>
 </template>
 
@@ -15,7 +35,7 @@ import "@/assets/styles/tailwind.css"
 export default {
     name: "Startup",
     components: {},
-    data() {
+    data: function() {
         return {
             stack: []
         };
